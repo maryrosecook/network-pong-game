462b334e346ca0efdb734bdfc56e3559ea2f7004
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-07-09 18:21:06 -0400

    Good style example.
    * Haven't changed this everywhere.  Good style is:
      Space between if and (
      Space between ) and {
      Space between } and else if
      Space between } and else
      Space between else and {

--- server.js
+++ server.js
@@ -20,11 +20,11 @@
 
 	var server = http.createServer(function(req, res){
 		var pathname = url.parse(req.url).pathname;
-		if(pathname == '/'){
+		if (pathname == '/') {
       serveStaticFile(__dirname + '/index.html', 'text/html', res);
-		}else if(pathname == '/game.js'){
+		} else if (pathname == '/game.js') {
       serveStaticFile(__dirname + '/game.js', 'text/javascript', res);
-		}else{
+		} else {
 			res.writeHead(400);
 			res.end('404 Not Found');
 		}
