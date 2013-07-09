41a113173167c8c623d80a18ca36d608720d27c9
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-07-09 18:41:53 -0400

    Group together event handling functions into eventHandlers.

--- server.js
+++ server.js
@@ -48,74 +48,76 @@ function init(){
 	setEventHandlers();
 }
 
+var eventHandlers = {
+  socketConnected: function(client){
+	  client.on('disconnect', eventHandlers.clientDisconnected);
+	  client.on('new player', eventHandlers.newPlayer);
+	  client.on('player moved', eventHandlers.playerMoved);
+  },
+
+  clientDisconnected: function(){
+	  //remove from players array
+	  var playerId = this.id;
+	  players.splice(findIndexById(playerId), 1);
+	  //broadcast to other players that client disconnected
+	  if(players.length > 1)
+		  this.broadcast.emit('player disconnected', {id: playerId});
+  },
+
+  newPlayer: function(data){
+	  if(players.length <= 2){
+		  //only set the canvasWidth and canvasHeight with the first player
+		  if(!players.length){
+			  canvasHeight = data.canvasHeight;
+			  canvasWidth = data.canvasWidth;
+
+			  ball = new Ball(canvasWidth, canvasHeight);
+		  }
+
+		  var playerId = this.id;
+		  players.push({id: playerId});
+
+		  var playerIndex = findIndexById(playerId);
+		  players[playerIndex].x = data.x;
+		  players[playerIndex].y = (players.length === 1? canvasHeight - data.height - 5: 5);
+		  players[playerIndex].width = data.width;
+		  players[playerIndex].height = data.height;
+		  players[playerIndex].score = 0;
+
+		  var nth = (players.length > 1? 2: 1);
+		  players[playerIndex].nth = nth;
+
+		  //broadcast to other players about new player
+		  if(players.length > 1){
+			  this.broadcast.emit('new player', {id: playerIndex, id2: playerId, x: data.x});
+		  }
+
+		  //send new player data about existing players
+		  for(var i = 0, maxPlayers = players.length; i < maxPlayers; i++){
+			  if(players[i].id !== playerId)
+				  this.emit('new player', {id: i, id2: players[i].id, x: players[i].x, nth: 1});
+		  }
+
+		  this.emit('assign player', {nth: nth});
+		  this.emit('create ball', {x: ball.x, y: ball.y});
+
+		  if(players.length > 1){
+			  time = Date.now();
+			  loop = setInterval(gameLoop, 50);
+		  }
+	  }
+  },
+
+  playerMoved: function(data){
+	  var playerId = this.id;
+	  var playerIndex = findIndexById(playerId);
+	  players[playerIndex].x = data.x;
+	  this.broadcast.emit('player moved', {x: data.x});
+  }
+};
+
 function setEventHandlers(){
-	socket.sockets.on('connection', socketConnected);
-}
-
-function socketConnected(client){
-	client.on('disconnect', clientDisconnected);
-	client.on('new player', newPlayer);
-	client.on('player moved', playerMoved);
-}
-
-function clientDisconnected(){
-	//remove from players array
-	var playerId = this.id;
-	players.splice(findIndexById(playerId), 1);
-	//broadcast to other players that client disconnected
-	if(players.length > 1)
-		this.broadcast.emit('player disconnected', {id: playerId});
-}
-
-function newPlayer(data){
-	if(players.length <= 2){
-		//only set the canvasWidth and canvasHeight with the first player
-		if(!players.length){
-			canvasHeight = data.canvasHeight;
-			canvasWidth = data.canvasWidth;
-
-			ball = new Ball(canvasWidth, canvasHeight);
-		}
-
-		var playerId = this.id;
-		players.push({id: playerId});
-
-		var playerIndex = findIndexById(playerId);
-		players[playerIndex].x = data.x;
-		players[playerIndex].y = (players.length === 1? canvasHeight - data.height - 5: 5);
-		players[playerIndex].width = data.width;
-		players[playerIndex].height = data.height;
-		players[playerIndex].score = 0;
-
-		var nth = (players.length > 1? 2: 1);
-		players[playerIndex].nth = nth;
-
-		//broadcast to other players about new player
-		if(players.length > 1){
-			this.broadcast.emit('new player', {id: playerIndex, id2: playerId, x: data.x});
-		}
-
-		//send new player data about existing players
-		for(var i = 0, maxPlayers = players.length; i < maxPlayers; i++){
-			if(players[i].id !== playerId)
-				this.emit('new player', {id: i, id2: players[i].id, x: players[i].x, nth: 1});
-		}
-
-		this.emit('assign player', {nth: nth});
-		this.emit('create ball', {x: ball.x, y: ball.y});
-
-		if(players.length > 1){
-			time = Date.now();
-			loop = setInterval(gameLoop, 50);
-		}
-	}
-}
-
-function playerMoved(data){
-	var playerId = this.id;
-	var playerIndex = findIndexById(playerId);
-	players[playerIndex].x = data.x;
-	this.broadcast.emit('player moved', {x: data.x});
+	socket.sockets.on('connection', eventHandlers.socketConnected);
 }
 
 function findIndexById(playerId){
